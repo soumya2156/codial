c42531d41c63d003daaff290ce2628dad449188b
Soumya Das <soumyasagardaswork@gmail.com>
2023-05-10 13:02:16 +0530

    Folder structure added

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port , function(err){
+    if(err){
+        console.log(`Error in running the Server: ${err}`);
+    }
+    console.log(`Server is up and running on PORT: ${port}`);
+});
