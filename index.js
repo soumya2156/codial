8aab6cf43aabf98a9e2b0df647a63d326ad47a8e
soumya2156 <soumyasagardaswork@gmail.com>
2023-05-16 16:48:14 +0530

    Sign up added

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
+
+//Setting of DB config
+const db = require('./config/mongoose');
+
+//seting of encoding and cookie parser
+app.use(express.urlencoded());
+app.use(cookieParser());
+
+const sign_up = require('./models/users');
+
+//include express ejs layouts
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
 
 //use express router
 app.use('/', require('./routes/index'));
